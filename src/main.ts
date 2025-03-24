a0b74b6e799d6f8b5283a8fc18b17fe9fba15fce
Simeon199 <sim.ki@outlook.de>
2025-03-24 19:27:38 +0100

    small test comment added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
+/* Small Test Comment added */
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
